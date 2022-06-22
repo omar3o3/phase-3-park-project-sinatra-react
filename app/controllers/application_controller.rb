db0ca6bd1dcec1f168500bb2a661f02492db2788
omar3o3 <otantawy53@gmail.com>
2022-06-22 11:27:30 -0400

    getting ready to create friend instance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,28 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   post "/add-event" do
-    # binding.pry
-    puts params
-    # YourEvent.create(
-      # event_borough: params[:event_borough],
-      # event_id: event.event_id,
-      # event_location: event.event_location,
-      # event_name: event.event_name,
-      # event_type: event.event_type,
-      # start_date_time: event.start_date_time,
-      # end_date_time: event.end_date_time,
-    # )
-  end
+    # puts params
+    # puts params[:event][:event_id]
+    # puts params[:event.event_location]
 
+    Borough.find_or_create_by(borough_name: params[:event][:event_borough])
+
+    EventType.find_or_create_by(event_type_name: params[:event][:event_type])
+
+    YourEvent.create(
+      event_id: params[:event][:event_id],
+      event_location: params[:event][:event_location],
+      event_name: params[:event][:event_name],
+      start_date_time: params[:event][:start_date_time],
+      end_date_time: params[:event][:end_date_time],
+      borough_id: Borough.find_by(borough_name: params[:event][:event_borough]).id,
+      event_type_id: EventType.find_by(event_type_name: params[:event][:event_type]).id
+    )
+
+    # Friend.create(
+    #   group_of_names: params[:friend_group]
+    #   your_event_id: YourEvent.all.find_by( = params[:])
+    # )
+
+  end
 end
